0f0ed0b3813805c1e4457f9e985168efe2b2f985
Ryan Tolboom <rxt1077@njit.edu>
2019-10-30 16:37:26 +0000

    Added nut_free attribute to Cookie

--- cookies/models.py
+++ cookies/models.py
@@ -4,4 +4,5 @@ class Cookie(models.Model):
     cookie_type = models.CharField(max_length=200)
     amount = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    nut_free = models.BooleanField(default=True)
 
